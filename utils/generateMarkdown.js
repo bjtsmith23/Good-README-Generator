a9fdb8425454771f20c791d69021dba5bbf813d5
Brian Smith <bjtsmith23@gmail.com>
2021-12-10 19:53:03 -0500

    updated utils file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -29,6 +29,7 @@ function renderLicenseBadge(license) {
   // TODO: Create a function to generate markdown for README
   function generateMarkdown(data) {
     return `# ${data.title}
+    
     ## Table of Contents
     * [Description](#description)
     * [Installation](#installation)
